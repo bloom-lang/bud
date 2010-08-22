b3841c388b2f9b36d7b65f836eec5229355b19d8
Peter Alvaro <palvaro@cs.berkeley.edu>
2010-08-22 16:41:22 +0000

    oops

---
+++ lib/bud/strat.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'bud'
+
+class Stratification < Bud
+  def initialize(ip, port)
+    super(ip,port)
+  end
+  
+  def state
+    table :depends_tc, ['head', 'body', 'neg']
+    table :cycle, ['predicate'], ['neg']
+    table :stratum_base, ['predicate', 'stratum']
+    table :stratum, ['predicate'], ['stratum']
+  end
+  
+  def declaration
+    strata[0] = rules {
+
+      depends_tc <= depends.map { |d| [d.head, d.body, d.neg] }
+      dj = join [depends, depends_tc], [depends.body, depends_tc.head]
+      depends_tc <= dj.map do |b, r|
+        if b.neg || r.neg
+          [b.head, r.body, true]
+        else
+          [b.head, r.body, false]
+        end
+      end
+
+      cycle <= depends_tc.map do |d|
+        if d.head == d.body
+          [d.head, d.neg]
+        end
+      end
+
+    }
+
+    strata[1] = rules {
+      stratum_base <= depends.map {|d1| [d1.body, 0] unless depends.map{|d2| [d2.head]}.include? d1.body} 
+      j = join [depends, stratum_base], [depends.body, stratum_base.predicate] 
+      stratum_base <= j.map do |d,s| 
+        if d.neg 
+          [d.head, s.stratum + 1]
+        else
+          [d.head, s.stratum] # unless stratum.map{|s2| [s2.predicate]}.include? d.head 
+        end
+      end
+
+    }
+    strata[2] = rules {
+      stratum <= stratum_base.group([stratum_base.predicate], max(stratum_base.stratum))
+    }
+  end
+end
+
