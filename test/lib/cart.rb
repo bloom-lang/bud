806a4c2a59fd68becf9cc7f53d1e8e1c5a812db0
Peter Alvaro <palvaro@cs.berkeley.edu>
2010-09-23 16:49:58 +0000

    tests, examples

--- test/lib/cart.rb
+++ test/lib/cart.rb
@@ -7,44 +7,75 @@ class BasicCartServer < Bud
     table :cart_action, ['session', 'item', 'action', 'reqid']
     table :action_cnt, ['session', 'item', 'action'] , ['cnt']
     table :status, ['session', 'item'], ['cnt']
-	
+    table :member, ['player']
+    table :acked, ['server', 'peer', 'reqid']
+
+    table :checkout_guard, ['server', 'client', 'session']
+
+    scratch :client_action, ['server', 'client', 'session', 'item', 'action', 'reqid']
     scratch :ac, ['session', 'item', 'action', 'reqid']
 
+    channel :ack, 0, ['server', 'peer', 'reqid']
     channel :action, 0, ['server', 'client', 'session', 'item', 'action', 'reqid']
     channel :checkout, 0, ['server', 'client', 'session']
+    channel :response, 0, ['client', 'server', 'session', 'item', 'cnt']
+    channel :tickler, 0, ['server']
   end
-  
-  def declaration
-    strata[0] = rules {
+ 
+  declare
+    def accumulate 
       # store actions against the "cart;" that is, the session.
       cart_action <= action.map { |c| [c.session, c.item, c.action, c.reqid] }
+
       # do I have to split the join-agg into 2 strata?
-      j = join [ cart_action, checkout ], [cart_action.session, checkout.session]
+      j = join [ cart_action, checkout_guard ], [cart_action.session, checkout_guard.session]
       ac <= j.map do | a, c | 
         [a.session, a.item, a.action, a.reqid] #if a.session = c.session
       end
-    }
 
-    strata[1] = rules {
+      checkout_guard <= checkout.map{|c| c}
+
       action_cnt <= ac.group([ac.session, ac.item, ac.action], count(ac.reqid))
       action_cnt <= ac.map{|a| [a.session, a.item, 'D', 0] unless ac.map{|c| [c.session, c.item] if c.action == "D"}.include? [a.session, a.item]}
+    end
 
-      status <= join([action_cnt, action_cnt, checkout]).map do |a1, a2, c| 
+  declare 
+    def acks
+      ack <+ action.map {|a| [a.client, a.server, a.reqid] }
+      acked <= ack.map{|a| a}
+    end
+
+  declare
+    def consider
+      status <= join([action_cnt, action_cnt, checkout_guard]).map do |a1, a2, c| 
         if a1.session == a2.session and a1.item == a2.item and a1.session == c.session and a1.action == "A" and a2.action == "D"
           [a1.session, a1.item, a1.cnt - a2.cnt] if (a1.cnt - a2.cnt) > 0
         end
       end
-    }
-  end
-end
+    end
+  declare 
+    def finish
+      response <= join([status, checkout_guard], [status.session, checkout_guard.session]).map do |s, c| 
+        [c.client, c.server, s.session, s.item. s.cnt]
+      end
+    end
+  declare
+    def replicate
+      action <+ join([action, member]).map do |a, m|
+        unless acked.map{|ac| [ac.peer, ac.reqid]}.include? [m.player, a.reqid]
+          [m.player, a.server, a.session, a.item, a.action, a.reqid]
+        end
+      end
 
-class CartClient < Bud
-  def state
-  
-  end
+      #checkout <+ join([checkout_guard, member]).map do |c, m|
+      #  [m.player, c.client, c.session]
+      #end
+    end
 
-  def declaration
-
-  end
+  declare
+    def client
+      action <+ client_action.map{|a| a}
+    end
+  # um
 end
 
--- test/tc_cart.rb
+++ test/tc_cart.rb
@@ -5,8 +5,16 @@ require 'lib/cart'
 
 class TestCart < Test::Unit::TestCase
   def advance(p)
-    assert_nothing_raised(RuntimeError) {p.tickle}
     sleep 1
+    send_channel(p, "tickler", ["#{p.ip}:#{p.port}"])
+  end
+
+  def send_channel(c, chan, payload)
+    EventMachine::connect(c.ip, c.port) do |c|
+      pl = ([chan, payload]).to_msgpack
+      c.send_data(pl)
+    end
+    
   end
   
   def test_cart
@@ -17,29 +25,34 @@ class TestCart < Test::Unit::TestCase
     program.run_bg
     sleep 1
 
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'meat', 'A', 123])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'beer', 'A', 124])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'diapers', 'A', 125])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'meat', 'D', 126])
 
-    program.action <+ [['localhost:12345', 'localhost:12345', 1234, 'meat', 'A', 123]]
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'beer', 'A', 124]]
-    program.action <+ [['localhost:12345', 'localhost:12345', 1234, 'diapers', 'A', 125]]
-    program.action <+ [['localhost:12345', 'localhost:12345', 1234, 'meat', 'D', 126]]
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'beer', 'A', 127])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'beer', 'A', 128])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'beer', 'A', 129])
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'beer', 'D', 130])
+  
 
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'beer', 'A', 127]]
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'beer', 'A', 128]]
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'beer', 'A', 129]]
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'beer', 'D', 130]]
-
-    program.checkout <+ [['localhost:12345', 'localhost:12345',1234]]
-
-    advance(program)
-
-
-    program.action <+ [['localhost:12345', 'localhost:12345',1234, 'papers', 'A', 131]]
+    send_channel(program, "checkout", ['localhost:12345', 'localhost:12345',1234])
 
     advance(program)
     advance(program)
+    advance(program)
+    advance(program)
+
+    send_channel(program, "action", ['localhost:12345', 'localhost:12345', 1234, 'papers', 'A', 131])
+
+    advance(program)    
+    advance(program)    
+    advance(program)    
+    advance(program)    
 
     assert_equal(2, program.status.length)
     program.status.each do |a|
+      print "item: #{a.inspect}\n"
       if a.item == "beer"
         assert_equal(3, a.cnt)
       elsif a.item == "diapers"
@@ -50,7 +63,7 @@ class TestCart < Test::Unit::TestCase
     end
 
     # the checkout message is redelivered!
-    program.checkout <+ [['localhost:12345', 'localhost:12345',1234]]
+    send_channel(program, "checkout", ['localhost:12345', 'localhost:12345',1234])
     advance(program)
     
 
