fbcd3416bb38d2b569d2d2a5e07cdf37d0c20337
Peter Alvaro <palvaro@cs.berkeley.edu>
2010-10-17 19:38:25 +0000

    eval

--- test/tc_evaluation.rb
+++ test/tc_evaluation.rb
@@ -3,6 +3,7 @@ require 'bud'
 require 'test/unit'
 
 class Paths < Bud
+  # a naive transitive closure of an edge relation.
   attr_reader :cnt, :pcnt
   def initialize(ip, port)
     @cnt = @pcnt = 0
@@ -27,7 +28,9 @@ class Paths < Bud
 end
 
 
-class SP2 < Paths
+class PathsDelta < Paths
+  # the same transitive closure, made seminaive with supplemental
+  # rules and relations
   attr_reader :m1cnt, :m2cnt
   def initialize(ip, port)
     @m1cnt = @m2cnt = 0
@@ -66,7 +69,46 @@ class SP2 < Paths
   end
 end
 
+class PathsDeltaIndirected < PathsDelta
+  def state
+    super
+    table :n_link, ['from', 'to']
+    table :n_path, ['from', 'to']    
+  end
+  
+  declare
+  def program
+    d_path <+ d_link.map{|e| (@cnt = @cnt + 1) and [e.from, e.to]}
 
+    j = join [d_link, n_path], [n_path.from, d_link.to]
+    j2 = join [n_link, d_path], [d_path.from, n_link.to]
+
+    d_path <+ j.map do |l,p|
+      (@pcnt = @pcnt + 1) and [l.from, p.to]
+    end
+
+    d_path <+ j2.map do |l,p|
+      (@pcnt = @pcnt + 1) and [l.from, p.to]
+    end
+  end
+
+  declare
+  def next_strat
+    n_path <+ d_path.map{|p| (@m1cnt = @m1cnt + 1) and p}
+    n_link <+ d_link.map{|l| (@m2cnt = @m2cnt + 1) and l}
+  end
+
+  declare
+  def alfinal
+    link <+ n_link.map{|n| n if d_link.empty? and d_path.empty?}
+    path <+ n_path.map{|n| n if d_link.empty? and d_path.empty?}
+
+    # need to clean up the intermediates
+    n_link <+ n_link.map{|n| n if d_link.empty? and d_path.empty?}
+    n_path <+ n_path.map{|n| n if d_link.empty? and d_path.empty?}
+
+  end
+end
 
 class TestDelta < Test::Unit::TestCase
   def inserts(tab)
@@ -87,7 +129,7 @@ class TestDelta < Test::Unit::TestCase
     
   end
 
-  def test_dr
+  def test_naive
     program = Paths.new('localhost', 12345)
     inserts(program.link)
     assert_nothing_raised( RuntimeError) { program.tick }
@@ -102,8 +144,7 @@ class TestDelta < Test::Unit::TestCase
     
   end
 
-  def test_handwrite
-    program = SP2.new('localhost', 12345)
+  def top_half(program)
     inserts(program.d_link)
     assert_nothing_raised( RuntimeError) { program.tick }
     # base case applications
@@ -111,24 +152,9 @@ class TestDelta < Test::Unit::TestCase
     assert_nothing_raised( RuntimeError) { program.tick }
     # (is quiesent)
     assert_equal(4, program.cnt)
+  end
 
-    # I am "seminaive"
-    iters = 0
-    while (program.d_link.length > 0 or program.d_path.length > 0) do  
-      assert_nothing_raised( RuntimeError) { program.tick }
-      iters = iters + 1
-      # however, intermediate results are visible at each step of iteration:
-      case iters
-        when 1 then assert_equal(program.path.length, 4)
-        when 2 then assert_equal(program.path.length, 7)
-        when 3 then assert_equal(program.path.length, 9)
-        when 4 then assert_equal(program.path.length, 10)
-      end
-    end
-
-    # 4 iterations were required to process a graph w/ diameter 4
-    assert_equal(4, iters)
-   
+  def bottom_half(program)
     # m1cnt: # of times a d_link tuple was 'stored' in link
     # m2cnt: # of times a d_path tuple was 'stored' in path
     # 40 times? huh? 
@@ -143,6 +169,42 @@ class TestDelta < Test::Unit::TestCase
     # same result as "naive": 10-4
     assert_equal(10, program.path.length)
     assert_equal(4, program.link.length)
+  end
 
+  def test_handwrite_delta
+    program = PathsDelta.new('localhost', 12345)
+    top_half(program)
+    # I am "seminaive"
+    iters = 0
+    while (program.d_link.length > 0 or program.d_path.length > 0) do  
+      assert_nothing_raised( RuntimeError) { program.tick }
+      iters = iters + 1
+      # however, intermediate results are visible at each step of iteration:
+      case iters
+        when 1 then assert_equal(program.path.length, 4)
+        when 2 then assert_equal(program.path.length, 7)
+        when 3 then assert_equal(program.path.length, 9)
+        when 4 then assert_equal(program.path.length, 10)
+      end
+    end
+    # 4 iterations were required to process a graph w/ diameter 4
+    assert_equal(4, iters)
+    bottom_half(program)
+  end
+
+  def test_handwrite_indirect_delta
+    program = PathsDeltaIndirected.new("localhost", 54321)
+    top_half(program)
+    # I am "seminaive"
+    iters = 0
+    while (program.d_link.length > 0 or program.d_path.length > 0) do
+      assert_nothing_raised( RuntimeError) { program.tick }
+      iters = iters + 1
+      # intermediate results are not visible.
+      assert_equal(0, program.path.length)
+    end
+    # 4 iterations were required to process a graph w/ diameter 4
+    assert_equal(4, iters)
+    bottom_half(program)
   end
 end
