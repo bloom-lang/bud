642c88055706c7d8591135f30476d6fcceb1b63e
Peter Alvaro <palvaro@cs.berkeley.edu>
2010-10-30 17:18:14 +0000

    better 2pc test

--- test/cart/test/tc_2pc.rb
+++ test/cart/test/tc_2pc.rb
@@ -7,14 +7,15 @@ class TestVoting < TestLib
   def test_singlenode
     t = TwoPCMaster.new('localhost', 12345, nil)
     t2 = TwoPCAgent.new('localhost', 12346, nil)
+    t3 = TwoPCAgent.new('localhost', 12347, nil)
     t.run_bg
     t2.run_bg
+    t3.run_bg
     t.member << ['localhost:12346']
+    t.member << ['localhost:12347']
     t.request_commit <+ [[ 1, "foobar" ]]
     advance(t)
     advance(t)
-    advance(t2)
-
 
     assert_equal(1, t.xact.length)
     assert_equal("prepare", t.xact.first[2])
@@ -25,6 +26,10 @@ class TestVoting < TestLib
     advance(t2)
     advance(t)
     assert_equal(1, t.master_vote_cache.length)
+
+    t3.cast_vote <+ [[ 1, "Y" ]]
+    advance(t3)
+    advance(t)
 
     advance(t)
     advance(t)
