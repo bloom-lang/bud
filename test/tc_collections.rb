c030379fd982a7e75e99054efd4a9c5f593d774f
Neil Conway <nrc@cs.berkeley.edu>
2011-01-23 17:31:59 -0800

    Whitespace cleanup.

--- test/tc_collections.rb
+++ test/tc_collections.rb
@@ -8,20 +8,20 @@ class BabyBud < Bud
     scratch :scrtch2, ['k1', 'k2']
     table :tbl, ['k1', 'k2'], ['v1', 'v2']
   end
-  
+
   def bootstrap
-      scrtch <= [['a', 'b', 1, 2]]
-      scrtch <= [['a', 'c', 3, 4]]
-      scrtch2 <= [['a', 'b']]
-      tbl <= [['a', 'b', 1, 2]]
-      tbl <= [['z', 'y', 9, 8]]
+    scrtch <= [['a', 'b', 1, 2]]
+    scrtch <= [['a', 'c', 3, 4]]
+    scrtch2 <= [['a', 'b']]
+    tbl <= [['a', 'b', 1, 2]]
+    tbl <= [['z', 'y', 9, 8]]
   end
-  
-  declare 
+
+  declare
   def rules
-      scrtch <+ [['c', 'd', 5, 6]] 
-      tbl <+ [['c', 'd', 5, 6]]
-      tbl <- [['a', 'b', 1, 2]] 
+    scrtch <+ [['c', 'd', 5, 6]]
+    tbl <+ [['c', 'd', 5, 6]]
+    tbl <- [['a', 'b', 1, 2]]
   end
 end
 
@@ -29,7 +29,7 @@ class DupKeyBud < Bud
   def state
     scratch :tab, ['k'], ['name']
   end
-  
+
   declare
   def program
     tab <= [[2000, 'bush']]
@@ -52,17 +52,17 @@ end
 
 class Grep < Bud
   attr_reader :pattern
-  
+
   def initialize(ip, port, pattern)
     super(ip,port)
     @pattern = pattern
   end
-  
+
   def state
     file_reader :text, '../examples/chap2/ulysses.txt'
     table :matches, ['lineno', 'text']
   end
-  
+
   declare
   def program
     matches <= text.map{|t| t if t.text =~ pattern}
@@ -75,12 +75,12 @@ class Union < Bud
     table :delta_link, ['from', 'to', 'cost']
     table :union, ['from', 'to', 'cost']
   end
-  
+
   def bootstrap
     link <= [['a', 'b', 1]]
     delta_link <= [['a', 'b', 4]]
   end
-  
+
   declare
   def prog
     union <= (delta_link <= link)
@@ -94,11 +94,11 @@ class TickleCount < Bud
     table   :result, ['nums']
     table   :mresult, ['nums']
   end
-  
+
   def bootstrap
     loopback <~ [[0]]
   end
-  
+
   declare
   def count_to_5
     loopback <~ loopback.map{|l| [l.cnt + 1] if l.cnt < 6 }
@@ -109,7 +109,7 @@ class TickleCount < Bud
 end
 
 class TestCollections < Test::Unit::TestCase
- 
+
   def test_simple_deduction
     program = BabyBud.new('localhost', 12345)
     assert_equal(2, program.scrtch.length)
@@ -118,13 +118,13 @@ class TestCollections < Test::Unit::TestCase
     assert_equal(0, program.scrtch2.length)
     assert_equal(2, program.tbl.length)
   end
-  
+
   def test_tuple_accessors
     program = BabyBud.new('localhost', 12345)
     assert_equal(1, program.scrtch[['a','b']].v1)
     assert_equal(2, program.scrtch[['a','b']].v2)
   end
-  
+
   def test_insert_delete
     program = BabyBud.new('localhost', 12345)
     # tick twice to get to 2nd timestep
@@ -134,11 +134,11 @@ class TestCollections < Test::Unit::TestCase
     assert_equal(0, program.scrtch2.length )
     assert_equal(2, program.tbl.length )
   end
-  
+
   def test_dup_tables
     assert_raise(Bud::BudError) {program = DupTableBud.new('localhost', 12345)}
   end
-  
+
   def test_dup_columns
     assert_raise(Bud::BudError) {program = DupColBud.new('localhost', 12345)}
   end
@@ -147,7 +147,7 @@ class TestCollections < Test::Unit::TestCase
     program = DupKeyBud.new('localhost', 12345)
     assert_raise(Bud::KeyConstraintError) { program.tick }
   end
-  
+
   def test_grep
     program = Grep.new('localhost', ARGV[0], /[Bb]loom/)
     assert_nothing_raised(RuntimeError) { program.tick }
@@ -155,14 +155,14 @@ class TestCollections < Test::Unit::TestCase
     assert_equal(1, lines.length)
     assert_equal(44, lines[0][0])
   end
-  
+
   def test_union
     s = Union.new('localhost', 12345)
     assert_nothing_raised(RuntimeError) { s.tick }
     assert_equal(2, s.union.length)
     assert_equal("[[\"a\", \"b\", 4], [\"a\", \"b\", 1]]", s.union.map{|t| t}.inspect)
   end
-  
+
   def test_tickle_count
     c = TickleCount.new('localhost', 12345)
     c.run_bg
