88a102cce776196aaa08e0a76a7097815280afcb
Neil Conway <nrc@cs.berkeley.edu>
2011-01-23 21:02:56 -0800

    Tweak Tokyo Cabinet tests.

--- test/tc_tc.rb
+++ test/tc_tc.rb
@@ -25,15 +25,23 @@ class TestTc < Test::Unit::TestCase
     assert_equal(2, t.t1.length)
   end
 
-  def test_key_conflict
+  def test_key_conflict_delta
     t = TcTest.new('localhost', 12346)
     t.in_buf << ['1', '2', '3', '4']
     t.in_buf << ['1', '2', '3', '5']
     assert_raise(Bud::KeyConstraintError) {t.tick}
   end
 
-  def test_key_merge
+  def test_key_conflict
     t = TcTest.new('localhost', 12347)
+    t.in_buf << ['1', '2', '3', '4']
+    assert_nothing_raised(RuntimeError) {t.tick}
+    t.in_buf << ['1', '2', '3', '5']
+    assert_raise(Bud::KeyConstraintError) {t.tick}
+  end
+
+  def test_key_merge
+    t = TcTest.new('localhost', 12348)
     t.in_buf << ['1', '2', '3', '4']
     t.in_buf << ['1', '2', '3', '4']
     t.in_buf << ['1', '2', '3', '4']
@@ -44,7 +52,12 @@ class TestTc < Test::Unit::TestCase
     assert_equal(3, t.t1.length)
   end
 
+  # TODO: check that each() works
   def test_scan
+  end
+
+  # TODO: check that stopping / restarting a Bud instance persists storage
+  def test_persist
   end
 
   def test_pending_ins
@@ -64,6 +77,7 @@ class TestTc < Test::Unit::TestCase
     assert_nothing_raised(RuntimeError) {t.tick}
     assert_equal(2, t.t1.length)
 
+    # XXX: fix this behavior
     if false
     t.del_buf << ['1', '2', '3', '5'] # shouldn't delete
     assert_nothing_raised(RuntimeError) {t.tick}
