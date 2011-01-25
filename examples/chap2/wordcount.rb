f395577bfa8beaf0dd3a74359d7797cecf68c5a1
Neil Conway <nrc@cs.berkeley.edu>
2011-01-25 00:45:37 -0800

    Code cleanup for example programs.

--- examples/chap2/wordcount.rb
+++ examples/chap2/wordcount.rb
@@ -3,13 +3,10 @@ require 'rubygems'
 require 'bud'
 
 class WordCount < Bud
-  attr_reader :pattern
-  
-  def initialize(ip, port, pattern)
-    super(ip,port)
-    @pattern = pattern
+  def initialize(ip, port)
+    super(ip, port)
   end
-  
+
   def state
     file_reader :text, 'ulysses.txt'
     table :words, ['lineno', 'wordno', 'word']
@@ -17,28 +14,26 @@ class WordCount < Bud
     table :wc2, ['word'], ['cnt']
     table :compare, ['word'], ['cnt', 'cnt2']
   end
-  
+
   def bootstrap
     text.each do |t|
       t.text.split.each_with_index {|w,i| words << [t.lineno, i, w]}
     end
   end
-  
-  def declaration
-    strata[0] = rules {
-      wc <= words.group([words.word], count)
-      wc2 <= words.reduce({}) do |memo, t|
-        memo[t.word] ||= 0
-        memo[t.word] += 1
-        memo
-      end
-      compare <= join([wc,wc2], [wc.word, wc2.word]).map { |w, w2| [w.word, w.cnt, w2.cnt] }
-    }
+
+  declare
+  def logic
+    wc <= words.group([words.word], count)
+    wc2 <= words.reduce({}) do |memo, t|
+      memo[t.word] ||= 0
+      memo[t.word] += 1
+      memo
+    end
+    compare <= join([wc,wc2], [wc.word, wc2.word]).map { |w, w2| [w.word, w.cnt, w2.cnt] }
   end
 end
 
-program = WordCount.new('localhost', ARGV[0], /[Bb]loom/)
-
+program = WordCount.new('localhost', ARGV[0])
 program.tick
 program.compare.each {|t| puts "mismatch: #{t.cnt} != #{t.cnt2}: #{t.inspect}" if t.cnt != t.cnt2}
-program.wc.sort{|x,y| x[1] <=> y[1] }.each {|t| puts t.inspect }
+program.wc.sort{|x,y| x[1] <=> y[1] }.each {|t| puts t.inspect}
