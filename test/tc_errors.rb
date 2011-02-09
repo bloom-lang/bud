04dd933405c954f5c0efbc68ed999ecbeadec107
Neil Conway <nrc@cs.berkeley.edu>
2011-02-09 15:01:31 -0800

    Add simple test case for error handling.

---
+++ test/tc_errors.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'bud'
+require 'test/unit'
+
+class TestErrorHandling < Test::Unit::TestCase
+  def test_do_sync_error
+    b = Bud.new
+    b.run_bg
+    3.times {
+      assert_raise(ZeroDivisionError) {
+        b.sync_do {
+          puts 5 / 0
+        }
+      }
+    }
+
+    b.stop_bg
+  end
+end
