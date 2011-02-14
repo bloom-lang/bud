387cbd1731e0a7f804dd361cf8eeb49b18c83952
Peter Alvaro <peteralvaro@shade.(none)>
2011-02-13 20:46:56 -0800

    tweaks

--- lib/bud/viz.rb
+++ lib/bud/viz.rb
@@ -9,7 +9,7 @@ class VizOnline
   def initialize(bud_instance)
     @bud_instance = bud_instance
     @meta_tables = {'t_rules' => 1, 't_depends' => 1, 't_table_info' => 1, 't_cycle' => 1, 't_stratum' => 1, 't_depends_tc' => 1}
-    @bud_instance.options[:tc_dir] = "#{@bud_instance.class}_#{@bud_instance.budtime}"
+    @bud_instance.options[:tc_dir] = "#{@bud_instance.class}_#{@bud_instance.port}_#{bud_instance.object_id}"
     @table_info = new_tab(:t_table_info, [:tab_name, :tab_type], @bud_instance)
 
     @logtab = {}
@@ -21,7 +21,7 @@ class VizOnline
       end
       lt = "#{t[0]}_log".to_sym
       @logtab[t[0]] = new_tab(lt, news, @bud_instance)
-      @table_info << [t[0], t[1].class]
+      @table_info << [t[0], t[1].class.to_s]
     end 
   end
 
@@ -37,9 +37,10 @@ class VizOnline
       next if tab.to_s =~ /_log\z/
       next if @meta_tables[tab.to_s] and @bud_instance.budtime > 0
       t[1].each do |row|
-        @logtab[tab] << row.clone.unshift(@bud_instance.budtime)
-        @logtab[tab].tick
+        newrow = row.clone.unshift(@bud_instance.budtime)
+        @logtab[tab] << newrow
       end
+      @logtab[tab].tick
     end
   end
 end
