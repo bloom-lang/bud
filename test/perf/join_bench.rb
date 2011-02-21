3575e764ec7b5caddaa6ad2152314d08f1b3c0be
Neil Conway <nrc@cs.berkeley.edu>
2011-02-20 21:01:53 -0800

    Simple hash join microbenchmark.

---
+++ test/perf/join_bench.rb
@@ -0,0 +1,40 @@
+require "rubygems"
+require "bud"
+require "benchmark"
+
+JOIN_INPUT_SIZE = 10000
+
+class HashJoinBench
+  include Bud
+
+  state do
+    table :t1
+    table :t2
+  end
+
+  def bootstrap
+    # Only a single pair of tuples satisfy the join condition
+    JOIN_INPUT_SIZE.times do |i|
+      t1 << [i, i + 50000]
+      t2 << [i + JOIN_INPUT_SIZE - 1, i]
+    end
+  end
+
+  declare
+  def do_join
+    j = join([t1, t2], [t1.key, t2.key])
+    stdio <~ j.map {|t1, t2| ["Join result: #{[t1,t2].inspect}"]}
+  end
+end
+
+b = HashJoinBench.new
+b.run_bg
+t = Benchmark.measure do
+  b.sync_do {
+    9.times do
+      b.tick
+    end
+  }
+end
+puts "Time taken for 10 joins: #{t}"
+b.stop_bg
