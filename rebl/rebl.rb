3074828c565063a18a100c79f1188f5f3bfa5980
billmarczak <>
2011-03-05 08:54:09 -0800

    rebellion rebooted

--- rebl/rebl.rb
+++ rebl/rebl.rb
@@ -1,95 +1,131 @@
-# XXX: parsing is horrible
-# XXX: catch signals
-# XXX: separate tick from print
-# XXX: allow rules or oneshot queries to be added "at the current time"
 require 'readline'
+require 'rubygems'
 require 'bud'
 
-# set up everything
-bud_class = Class.new(Bud)
-bud_class_instance = bud_class.new
-bud_class_instance.instance_eval("@declarations << :rules")
+TABLE_TYPES = ["table", "scratch"]
+BUILTIN_TABLES = [:stdio, :t_depends, :periodics_tbl, :t_cycle, :localtick,
+                  :t_provides, :t_rules, :t_depends_tc, :t_stratum]
+HISTFILE = "~/.rebl_history"
+MAXHISTSIZE = 100
 
-def bud_class_instance.safe_instance_eval(str)
-  begin
-    self.instance_eval(str)
-  rescue Exception
-    puts "#{$!}"
-    return false
-  end
-  return true
+# set up everything
+class ReblClass
+  include Bud
 end
 
-bud_class_instance.instance_eval("def print_state\nend")
-bud_class_instance.instance_eval("state {}")
+def reinstantiate
+  # new anonymous subclass
+  @rebl_class = Class.new(ReblClass)
 
-table_names = []
-state_exprs = []
-rules = []
-
-loop do
-  line = Readline::readline('> ').lstrip
-  Readline::HISTORY.push(line)
-
-  if line[0..0] == "/" then
-    if line[1..4] == "tick" then
-      bud_class_instance.tick
-      bud_class_instance.print_state
-    end
-    next
+  if not @rules.empty?
+    @rebl_class.class_eval("declare\ndef rebl_rules\n" +
+                           @rules.values.join("\n") + "\nend")
   end
 
-  # XXX: expand to other types of tables
-  if line[0..5] == "table " then
-    next if not bud_class_instance.safe_instance_eval(line)
-    state_exprs << line
+  if not @state.empty?
+    @rebl_class.class_eval("state do\n" + @state.values.join("\n") + "\nend")
+  end
 
-    # 100% hacky parsing
-    table_name = line.split(" ")[1]
-    table_name.slice!(0)
-    table_name = table_name.chop
-    table_names << table_name
+  # instantiate it
+  @old_inst = @rebl_class_inst
+  @rebl_class_inst = @rebl_class.new
 
-    # remove state & print_state
-    class << bud_class_instance
-      remove_method(:print_state)
-      remove_method(:state)
+  # copy the tables over
+  if @old_inst
+    @rebl_class_inst.tables.merge!(@old_inst.tables.reject do |k,v|
+                                     BUILTIN_TABLES.include? k
+                                   end)
+  end
+end
+
+def setup_history
+  # permament history; code lifted from irb
+  begin
+    histfile = File::expand_path(HISTFILE)
+    if File::exists?(histfile)
+      lines = IO::readlines(histfile).collect { |line| line.chomp }
+      Readline::HISTORY.push(*lines)
     end
+    Kernel::at_exit do
+      lines = Readline::HISTORY.to_a.reverse.uniq.reverse
+      lines = lines[-MAXHISTSIZE, MAXHISTSIZE] if lines.nitems > MAXHISTSIZE
+      File::open(histfile, File::WRONLY|File::CREAT|File::TRUNC) { |io| io.puts lines.join("\n") }
+    end
+  rescue Exception
+    puts "Error when configuring permanent history: #{$!}"
+  end
+end
 
-    # add them back with new state
-    next if not bud_class_instance.safe_instance_eval("state do\n" + state_exprs.join("\n") + "\nend")
+# main
+@rules = {}
+@ruleid = 0
+@state = {}
+@stateid = 0
+@new_table = nil
+reinstantiate
+setup_history
 
-    def_print_state = "def print_state\n"
-    table_names.each{|t| def_print_state += "puts \"" + t + ":\n\"\n" + t + ".each {|t| puts t.inspect}\n"}
-    next if not bud_class_instance.safe_instance_eval(def_print_state + "end")
+begin
+  histfile = File::expand_path(HISTFILE)
+  if File::exists?(histfile)
+    lines = IO::readlines(histfile).collect { |line| line.chomp }
+    Readline::HISTORY.push(*lines)
+  end
+  Kernel::at_exit do
+    lines = Readline::HISTORY.to_a.reverse.uniq.reverse
+    lines = lines[-MAXHISTSIZE, MAXHISTSIZE] if lines.nitems > MAXHISTSIZE
+    File::open(histfile, File::WRONLY|File::CREAT|File::TRUNC) { |io| io.puts lines.join("\n") }
+  end
+rescue Exception
+  puts "Error when configuring permanent history: #{$!}"
+end
 
-  else
+loop do
+  begin
+    line = Readline::readline('rebl> ').lstrip.rstrip
+    Readline::HISTORY.push(line)
+    split_line = line.split(" ")
 
-    # do a dry run of inserting the block with the new rule, a do_rewrite, and a tick
-    rules << line
-    # XXX: NOT a deep copy! (but seems to work)
-    temp_bud_instance = bud_class_instance.clone
-    if temp_bud_instance.safe_instance_eval("def rules\n\t" + rules.join("\n") + "\nend")
-      begin
-        # even if our code passes the dry run, it might still fail on tick
-        temp_bud_instance.do_rewrite
-        # so let's tick that ho
-        # XXX: don't know if this will catch all potential problems
-        #      might still find yourself in an unrecoverable state
-        #      due to this statement even if tick succeeds
-        temp_bud_instance.tick
-      rescue Exception
-        puts "#{$!}"
-        rules.pop()
-        next
+    # command
+    if line[0..0] == "/" then
+      split_line[0].slice! 0
+      case split_line[0]
+      when "tick" then @rebl_class_inst.tick
+      when "lsrules" then puts @rules.inspect
+      when "rmrule"
+        @rules.delete(Integer(split_line[1]))
+        reinstantiate
+      when "lscollections"
+        puts @rebl_class_inst.tables.keys.find_all{ |tn| not BUILTIN_TABLES.include? tn}.inspect
+      when "dump" then @rebl_class_inst.instance_eval("#{split_line[1]}.dump")
+      else puts "invalid command"
       end
-    else
-      rules.pop()
       next
     end
 
-    # if we've succeeded with the dry run, then it's time to commit the new rule:
-    bud_class_instance.safe_instance_eval("def rules\n\t" + rules.join("\n") + "\nend")
-    bud_class_instance.do_rewrite
+    # collection
+    if TABLE_TYPES.include? split_line[0]
+      @state[@stateid += 1] = line
+      begin
+        reinstantiate
+      rescue Exception
+        @state.delete(@stateid)
+        raise
+      end
+    else # assume it's a rule
+      @rules[@ruleid += 1] = line
+      begin
+        reinstantiate
+      rescue Exception
+        @rules.delete(@ruleid)
+        raise
+      end
+    end
+    next
+
+  rescue SystemExit, Interrupt
+    abort("\nrebellion quashed")
+  rescue Exception
+    puts "exception: #{$!}"
   end
 end
