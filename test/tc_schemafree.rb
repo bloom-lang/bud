0fe789d9b9e017c0e11b321644904384240ca5c2
Neil Conway <nrc@cs.berkeley.edu>
2011-03-20 17:48:12 -0700

    Code cleanup for tc_schemafree test.

--- test/tc_schemafree.rb
+++ test/tc_schemafree.rb
@@ -13,22 +13,21 @@ class SchemaFree
   bloom do
     notes <= msgs.payloads
     msgs <~ send_me
-    # stdio <~ msgs.inspected
   end
 end
 
 class TestSFree < Test::Unit::TestCase
   def test_bloom
-    p = SchemaFree.new({:port=>54321})
-    assert_nothing_raised(RuntimeError) { p.run_bg }
-    assert_nothing_raised(RuntimeError) { p.sync_do {
-      p.send_me <+ [['127.0.0.1:54321', [[123, 1], 'what a lovely day']]]
-    }}
-    assert_nothing_raised(RuntimeError) { p.sync_do {
-      p.send_me <+ [['127.0.0.1:54321', [[123, 2], 'I think I\'ll go for a walk']]]
-    }}
+    p = SchemaFree.new
+    p.run_bg
+    p.sync_do {
+      p.send_me <+ [[p.ip_port, [[123, 1], 'what a lovely day']]]
+    }
+    p.sync_do {
+      p.send_me <+ [[p.ip_port, [[123, 2], "I think I'll go for a walk"]]]
+    }
     sleep 1
-    assert_nothing_raised(RuntimeError) { p.stop_bg }    
+    p.stop_bg
     assert_equal(2, p.notes.length)
     assert_equal(123, p.notes.first.key[0])
     assert_equal('what a lovely day', p.notes.first.val)    
