06c2f8e1a88e70cb0a46347739e52dc991cc11d1
Neil Conway <nrc@cs.berkeley.edu>
2011-03-29 21:52:39 -0700

    Whitespace cleanup.

--- lib/bud/depanalysis.rb
+++ lib/bud/depanalysis.rb
@@ -14,7 +14,7 @@ class DepAnalysis #:nodoc: all
   end
 
   def declaration
-    strata[0] = lambda { 
+    strata[0] = lambda {
       source <= providing do |p|
         if p.input and !depends_tc.map{|d| d.head}.include? p.pred
           [p.pred]
@@ -29,15 +29,15 @@ class DepAnalysis #:nodoc: all
 
       underspecified <= providing do |p|
         if p.input
-          unless depends_tc.map{|d| d.body if d.head != d.body}.include? p.pred 
+          unless depends_tc.map{|d| d.body if d.head != d.body}.include? p.pred
             [p.pred, true]
           end
-        else 
-          unless depends_tc.map{|d| d.head if d.head != d.body}.include? p.pred 
+        else
+          unless depends_tc.map{|d| d.head if d.head != d.body}.include? p.pred
             [p.pred, false]
           end
         end
-      end  
+      end
     }
   end
 end
