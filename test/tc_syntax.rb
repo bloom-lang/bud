1d6a3fe4c7091ea06ad1b9002a327d748e796896
Neil Conway <nrc@cs.berkeley.edu>
2011-04-11 20:08:51 -0700

    Report finer-grained syntax errors.
    Rather than just dumping the source code for the rule block that contained the
    syntax error, try to dump the code for the erroneous rule.

--- test/tc_syntax.rb
+++ test/tc_syntax.rb
@@ -24,7 +24,7 @@ class TestSyntax < Test::Unit::TestCase
     rescue
       assert_equal(Bud::CompileError, $!.class)
       # fragile assertion? (whitespace etc)
-      assert_equal("Error parsing rule block __bloom__foobar:\ndef __bloom__foobar\n  foo = bar\nend", $!.to_s)
+      assert_equal("Illegal operator: '=' in rule block \"__bloom__foobar\"\nCode: foo = bar", $!.to_s)
     end
   end
 end
