633ac3ddf817086ea9dc37dec0350643976356d6
Joe Hellerstein <jmh@berkeley.edu>
2011-04-12 22:05:39 -0400

    merge wordcount tests

--- test/tc_wc.rb
+++ test/tc_wc.rb
@@ -32,3 +32,40 @@ class TestWC1 < Test::Unit::TestCase
     assert_equal(23, program.wc[["yes"]].cnt)
   end
 end
+
+
+class WordCount2
+  include Bud
+
+  attr_reader :pattern
+
+  def initialize(pattern)
+    super()
+    @pattern = pattern
+  end
+
+  state do
+    file_reader :txt, 'text/ulysses.txt'
+    scratch :words, [:lineno, :wordno] => [:word]
+    scratch :wc, [:word] => [:cnt]
+  end
+
+  bloom do
+    words <= txt.flat_map do |t|
+      t.text.split.enum_for(:each_with_index).map {|w, i| [t.lineno, i, w]}
+    end
+    wc <= words.reduce({}) do |memo, t|
+      memo[t.word] ||= 0
+      memo[t.word] += 1
+      memo
+    end
+  end
+end
+
+class TestWC2 < Test::Unit::TestCase
+  def test_wc2
+    program = WordCount2.new(/[Bb]loom/)
+    assert_nothing_raised { program.tick }
+    assert_equal(23, program.wc[["yes"]].cnt)
+  end
+end
