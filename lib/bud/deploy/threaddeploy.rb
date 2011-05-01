bd22c5a93c97d18e7909c8fcc4bf728df1668742
Neil Conway <nrc@cs.berkeley.edu>
2011-05-01 00:10:16 -0700

    Adjust thread-based deployment to use on_shutdown callback.

--- lib/bud/deploy/threaddeploy.rb
+++ lib/bud/deploy/threaddeploy.rb
@@ -1,9 +1,9 @@
 require 'bud/deploy/deployer'
 
 # An implementation of the Deployer that runs instances using the current Ruby
-# process (listening at 127.0.0.1 on an ephemeral port). ThreadDeploy is
-# probably not the best name: all the spawned instances are run by a single
-# thread, they are just multiplexed via EventMachine.
+# process (listening on an ephemeral port). ThreadDeploy is probably not the
+# best name: all the spawned instances are run by a single thread, they are just
+# multiplexed via EventMachine.
 #
 # Note that this module is included in both the deployer process and in the
 # deployed instances. To write code that only runs in one type of process,
@@ -11,15 +11,14 @@ require 'bud/deploy/deployer'
 module ThreadDeploy
   include Deployer
 
-  def stop_bg
-    super
-    return unless @options[:deploy]
-    @instances.each {|b| b.stop_bg}
-  end
-
   bootstrap do
     return unless @options[:deploy]
+
     @instances = []
+    on_shutdown do
+      @instances.each {|b| b.stop_bg}
+    end
+
     print "Spawning threads"
     child_opts = @options[:deploy_child_opts]
     child_opts ||= {}
@@ -27,7 +26,7 @@ module ThreadDeploy
       b = self.class.new(child_opts)
       b.run_bg
       @instances << b
-      node << [i, "localhost:#{b.port}"]
+      node << [i, b.ip_port]
       print "."
     end
     puts "done"
