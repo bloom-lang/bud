c366d4a9652b8b5c406259b886d74d590ecabe24
Neil Conway <nrc@cs.berkeley.edu>
2011-05-05 13:36:25 -0700

    Refactor test case.

--- test/tc_coverage.rb
+++ test/tc_coverage.rb
@@ -15,22 +15,21 @@ end
 
 # lib/bud.rb
 class GratuitousBud < Test::Unit::TestCase
-  def test_sigint
+  def kill_child_with_sig(signal)
     pid = fork do
       p = Nada.new
       p.run_fg
     end
     sleep 1
-    Process.kill("INT", pid)
+    Process.kill(signal, pid)
     Process.waitpid(pid)
   end
+
+  def test_sigint
+    kill_child_with_sig("INT")
+  end
+
   def test_sigterm
-    pid = fork do
-      p = Nada.new
-      p.run_fg
-    end
-    sleep 1
-    Process.kill("TERM", pid)
-    Process.waitpid(pid)
+    kill_child_with_sig("TERM")
   end
 end
