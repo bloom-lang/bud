f9877737bc0d35fa286a9a6ab5139e4cd050997f
Neil Conway <nrc@cs.berkeley.edu>
2011-05-07 23:40:35 -0700

    Code cleanup for tokenring.

--- examples/deploy/tokenring.rb
+++ examples/deploy/tokenring.rb
@@ -27,7 +27,12 @@ module TokenRing
     token_persist <= token
     token_persist <- (token_persist * next_node).lefts
     # Pass on the token
-    token <~ (token_persist * next_node).pairs {[next_node[[]].addr]}
+    token <~ (token_persist * next_node).pairs do |tp, nn|
+      [nn.addr]
+    end
+  end
+
+  bloom :print_token do
     stdio <~ token {["#{ip_port}: Got token!"]}
   end
 end
