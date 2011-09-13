53cbbcc940ba5942c2a4e78feded5df4b2b80145
Neil Conway <nrc@cs.berkeley.edu>
2011-09-13 11:51:14 -0700

    Improve test case for lattices.

--- test/tc_lattice.rb
+++ test/tc_lattice.rb
@@ -1,6 +1,6 @@
 require 'test_common'
 
-class IMax
+class SimpleMax
   include Bud
 
   state do
@@ -15,12 +15,34 @@ class IMax
   end
 end
 
+class MaxOfMax
+  include Bud
+
+  state do
+    max :m1
+    max :m2
+    max :m3
+    scratch :s, [:val]
+    scratch :done, [:t]
+  end
+
+  bloom do
+    m1 <= s {|t| t if t.val % 2 == 0}
+    m2 <= s {|t| t if t.val % 2 == 1}
+    m3 <= m1
+    m3 <= m2
+    done <= Max.gt_k(10) {|t| [true]}
+  end
+end
+
 class TestMaxLattice
-  def test_imax
-    i = IMax.new
+  def test_simple_max
+    i = SimpleMax.new
     i.s <+ [[1], [2], [3]]
     i.tick
     assert(i.done.empty?)
   end
-end
 
+  def test_max_of_max
+  end
+end
