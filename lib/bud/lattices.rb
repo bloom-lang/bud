2d66ef21ed1ad6f7ea5227da433f0321d91c1084
Neil Conway <nrc@cs.berkeley.edu>
2011-11-28 14:49:22 -0800

    Checkpoint.

--- lib/bud/lattices.rb
+++ lib/bud/lattices.rb
@@ -26,6 +26,10 @@ class Bud::Lattice
     return if i.nil?
     @wrapper.merge_pending(i)
   end
+
+  def reveal
+    @v
+  end
 end
 
 class Bud::LatticeWrapper
@@ -43,35 +47,53 @@ class Bud::LatticeWrapper
   end
 
   def current_value
-    @storage ||= klass.new(self)
+    @storage ||= @klass.new(self)
     @storage
   end
 
+  private
   def current_delta
     @delta ||= current_value
     @delta
   end
 
   def current_pending
-    @pending ||= klass.new(self)
+    @pending ||= @klass.new(self)
     @pending
   end
 
+  def do_merge(lhs, rhs)
+    raise Bud::Error unless lhs.class <= Bud::Lattice
+
+    if rhs.class <= Enumerable
+      rhs.each do |r|
+        next if r.nil?
+        lhs = scalar_merge(lhs, @klass.new(self, r))
+      end
+      return lhs
+    end
+
+    scalar_merge(lhs, rhs)
+  end
+
+  def do_scalar_merge(lhs, r)
+    rv = lhs.merge(r)
+    @got_delta = true if rv.reveal != lhs.reveal
+    rv
+  end
+
+  public
   def merge(i)
-    prev_delta = current_delta
-    @delta = prev_delta.merge(i)
+    @delta = do_merge(current_delta, i)
   end
 
   def merge_pending(i)
-    prev_pending = current_pending
-    @pending = prev_pending.merge(i)
+    @pending = do_merge(current_pending, i)
   end
 
   def tick
-    if @is_scratch
-      @storage = nil
-    end
-    @storage = current_value.merge(@pending)
+    @storage = nil if @is_scratch
+    @storage = do_merge(current_value, @pending)
     @pending = nil
     raise Bud::Error unless @delta.nil?
   end
@@ -87,5 +109,13 @@ class Bud::MaxLattice < Bud::Lattice
 
   def merge(i)
     [@v, i.reveal].max
+  end
+end
+
+class Bud::MinLattice < Bud::Lattice
+  lattice_name :lat_min
+
+  def merge(i)
+    [@v, i.reveal].min
   end
 end
