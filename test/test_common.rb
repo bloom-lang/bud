7b29c68a9d75a56b3d32e5a01c9311516755377b
Neil Conway <nrc@cs.berkeley.edu>
2012-07-22 15:25:41 -0700

    Add built-in support for code coverage via SimpleCov.
    rcov no longer works with MRI 1.9

--- test/test_common.rb
+++ test/test_common.rb
@@ -1,8 +1,16 @@
+require 'rubygems'
+
+if ENV["COVERAGE"]
+  require 'simplecov'
+  SimpleCov.command_name 'minitest'
+  SimpleCov.root '../'
+  SimpleCov.start
+end
+
 # Prefer Bud from local source tree to any version in RubyGems
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 $:.unshift "."
 require 'bud'
 
-require 'rubygems'
 gem 'minitest'  # Use the rubygems version of MT, not builtin (if on 1.9)
 require 'minitest/autorun'
