63a3f0e2c0b9503e1007342e0b74bde740ab3b81
Neil Conway <nrc@cs.berkeley.edu>
2013-03-01 10:31:12 -0800

    Update release notes.
    Per 331833a3f974021780d6cc39d4ed8f75c7e2ffd4; closes #289.

--- lib/bud/collections.rb
+++ lib/bud/collections.rb
@@ -600,8 +600,7 @@ module Bud
     public
     # instantaneously merge items from collection +o+ into +buf+
     def <=(collection)
-      toplevel = bud_instance.toplevel
-      if not toplevel.inside_tick
+      unless bud_instance.toplevel.inside_tick
         raise Bud::CompileError, "illegal use of <= outside of bloom block, use <+ instead"
       end
 
