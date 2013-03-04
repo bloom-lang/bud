227d164d2f0a911e004fbe5b5266d38d6490e0a8
Neil Conway <nrc@cs.berkeley.edu>
2013-03-04 09:10:17 -0800

    Add test case for stdio.
    Check that reading from / writing to pipe IO objects works. This functionality
    is being used by the UCLA folks, so it seems prudent to verify that it works.

--- test/tc_terminal.rb
+++ test/tc_terminal.rb
@@ -13,6 +13,14 @@ class StdinReader
   end
 end
 
+class StdioEcho
+  include Bud
+
+  bloom do
+    stdio <~ stdio {|s| ["Saw: #{s.line}"]}
+  end
+end
+
 class TestTerminal < MiniTest::Unit::TestCase
   def test_stdin
     input_lines = ["line1", "line2", "line3"]
@@ -27,6 +35,22 @@ class TestTerminal < MiniTest::Unit::TestCase
     rv = []
     input_lines.length.times { rv << q.pop }
     assert_equal(input_lines.map{|l| [l]}.sort, rv.sort)
+    b.stop
+  end
+
+  def test_stdio_pipe
+    in_read, in_write = IO.pipe
+    out_read, out_write = IO.pipe
+
+    b = StdioEcho.new(:stdin => in_read, :stdout => out_write)
+    b.run_bg
+
+    ["foo", "bar", "baz"].each do |str|
+      in_write.puts(str)
+      rv = out_read.gets
+      assert_equal("Saw: #{str}\n", rv)
+    end
+
     b.stop
   end
 end
