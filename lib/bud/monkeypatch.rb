bloom-lang/bud

commit a3de27244f0b3d13636d4f517b1a6bee89c9700c

Cleanup and improve Struct/Array monkeypatching.
Rather than monkeypatching the builtin Struct class (kludgy), we instead define
a subclass Bud::TupleStruct; this is what we use to create the structs needed
for tuples in Bud collections. Hence we can safely monkeypatch Bud::TupleStruct
without risk of changing the semantics of other Structs used by unrelated code.

Along the way, have Bud::TupleStruct include Comparable (since we define a `<=>`
method anyway).

Note that I didn't fix a few issues. First, putting the custom equality method
of TupleStruct is inconsistent with its hashing behavior, since the latter is
defined by #hash and #eql? (which we don't override). To make this consistent,
we'd want TupleStruct#hash to match Array#hash -- but the easiest way to do that
would be to implement TupleStruct#hash as self.to_a.hash, which would have a
performance cost. I also noticed that TupleStruct#<=> is inconsistent with
TupleStruct#== (when comparing TupleStruct and Array instances); that should
probably be fixed.

Close #284.

lib/bud/monkeypatch.rb
@@ -10,9 +10,10 @@ class Class
   end
 end
 
-# FIXME: Use a subclass of Struct.
-# FIXME: Should likely override eql? as well
-class Struct
+# FIXME: Should likely override #hash and #eql? as well.
+class Bud::TupleStruct < Struct
+  include Comparable
+
   def <=>(o)
     if o.class == self.class
       self.each_with_index do |e, i|
@@ -27,16 +28,13 @@ class Struct
     end
   end
 
-  alias oldeq :==
   def ==(o)
     if o.class == self.class
-      return oldeq(o)
+      return super
     elsif o.class == Array
       begin
         self.each_with_index do |el, i|
-          if el != o[i]
-            return false
-          end
+          return false if el != o[i]
         end
         return true
       rescue StandardError
@@ -62,9 +60,7 @@ end
 class Array
   alias :oldeq :==
   def ==(o)
-    if o.kind_of? Struct
-      o = o.to_a
-    end
+    o = o.to_a if o.kind_of? Bud::TupleStruct
     self.oldeq(o)
   end
 end
@@ -125,7 +121,6 @@ class Module
     @bud_import_tbl ||= {}
     @bud_import_tbl
   end
-
 
   # the block of Bloom collection declarations.  one per module.
   def state(&block)

lib/bud/collections.rb
@@ -63,7 +63,7 @@ module Bud
       if @cols.empty?
         @cols = nil
       else
-        @struct = ($struct_classes[@cols] ||= Struct.new(*@cols))
+        @struct = ($struct_classes[@cols] ||= Bud::TupleStruct.new(*@cols))
         @structlen = @struct.members.length
       end
       setup_accessors
@@ -409,7 +409,7 @@ module Bud
     private
     def prep_tuple(o)
       return o if o.class == @struct
-      if o.class == Array
+      if o.kind_of? Array
         if @struct.nil?
           sch = (1 .. o.length).map{|i| "c#{i}".to_sym}
           init_schema(sch)
@@ -1023,7 +1023,7 @@ module Bud
       if @payload_struct.nil?
         payload_cols = cols.dup
         payload_cols.delete_at(@locspec_idx)
-        @payload_struct = Struct.new(*payload_cols)
+        @payload_struct = Bud::TupleStruct.new(*payload_cols)
         @payload_colnums = payload_cols.map {|k| cols.index(k)}
       end
 

test/tc_collections.rb
@@ -813,3 +813,32 @@ class TestEachWithIndex < MiniTest::Unit::TestCase
     assert_equal([100, 101, 102].to_set, new_res_b.map {|t| t.idx}.to_set)
   end
 end
+
+class TestBudStruct < MiniTest::Unit::TestCase
+  MyStruct = Bud::TupleStruct.new(:x, :y)
+
+  def test_struct_hashing
+    v1 = MyStruct.new(1, 2)
+    v2 = MyStruct.new(1, 2)
+    assert_equal(v1, v2)
+    assert_equal([1, 2], v1)
+
+    h = {}
+    h[v1] = 1
+    assert(h.has_key? v2)
+  end
+
+  def test_struct_comparable
+    v1 = MyStruct.new(1, 2)
+    v2 = MyStruct.new(1, 3)
+    v3 = MyStruct.new(5, 1)
+    v4 = MyStruct.new(9, 0)
+
+    assert_equal(0, v1 <=> v1)
+    assert_equal(-1, v1 <=> v2)
+    assert_equal(1, v2 <=> v1)
+    assert(v1 < v2)
+    assert(v2 > v1)
+    assert_equal([v1, v2, v3, v4], [v2, v4, v3, v1].sort)
+  end
+end

commit 4920cd19c4b53a5e55f8c8eb29cc8dcd32fc1013

Fix MRI 1.8 regression.
A subtle MRI 1.8 compatibility issue was introduced in
a3de27244f0b3d13636d4f517b1a6bee89c9700c. Now that TupleStruct is a Comparable,
apparently <=> will be used inplace of == in a few places. Furthermore, `nil <=>
x` in MRI 1.8 produces `NoMethodError` in 1.8 but `nil` in MRI 1.9. This lead to
strange behavior.

Fix the regression and make the behavior more consistent in general by changing
TupleStruct#<=> to return `nil` on `nil` elements.

lib/bud/monkeypatch.rb
@@ -17,12 +17,15 @@ class Bud::TupleStruct < Struct
   def <=>(o)
     if o.class == self.class
       self.each_with_index do |e, i|
-        cmp = e <=> o[i]
-        return cmp if cmp != 0
+        other = o[i]
+        next if e == other
+        return nil if e.nil?
+        return nil if other.nil?
+        return e <=> other
       end
       return 0
     elsif o.nil?
-      return -1
+      return nil
     else
       raise "Comparison (<=>) between #{o.class} and #{self.class} not implemented"
     end