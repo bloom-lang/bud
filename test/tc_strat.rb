e1a685d122df392a1bb8f4770c57e57cc6c93a7b
Neil Conway <nrc@cs.berkeley.edu>
2014-01-15 11:02:11 -0800

    Add test skeleton.

---
+++ test/tc_strat.rb
@@ -0,0 +1,32 @@
+require './test_common'
+
+class SimpleStrat
+  include Bud
+
+  state do
+    table :t1
+    table :t2
+    table :t3
+  end
+
+  bloom do
+    t2 <= t1 {|t| [t.key + 1, t.val + 1]}
+    t3 <= t2.group([:key], max(:val))
+  end
+end
+
+class PartHierarchy
+  include Bud
+
+  state do
+  end
+
+  bloom do
+  end
+end
+
+class TestStrat < MiniTest::Unit::TestCase
+  def test_simple_strat
+    s = SimpleStrat.new
+  end
+end
