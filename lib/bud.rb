97f8c412b03ba1cc22c029597c197a4dc3328b77
Neil Conway <nrc@cs.berkeley.edu>
2014-02-19 11:32:22 -0800

    Fix error in rescan/invalidation logic for nested notin.

--- lib/bud.rb
+++ lib/bud.rb
@@ -495,8 +495,6 @@ module Bud
       rescan_invalidate_tc(stratum, rescan, invalidate)
     end
 
-    prune_rescan_invalidate(rescan, invalidate)
-    # transitive closure
     @default_rescan = rescan.to_a
     @default_invalidate = invalidate.to_a
 
@@ -521,7 +519,7 @@ module Bud
       rescan = dflt_rescan.clone
       invalidate = dflt_invalidate + [scanner.collection]
       rescan_invalidate_tc(stratum, rescan, invalidate)
-      prune_rescan_invalidate(rescan, invalidate)
+      prune_rescan_set(rescan)
 
       # Make sure we reset the rescan/invalidate flag for this scanner at
       # end-of-tick, but we can remove the scanner from its own
@@ -594,7 +592,7 @@ module Bud
     end
   end
 
-  def prune_rescan_invalidate(rescan, invalidate)
+  def prune_rescan_set(rescan)
     rescan.delete_if {|e| e.rescan_at_tick}
   end
 
